612592e0185484cffeae8ac584590b3225e371e8
Pedro Sala <pedrosalamora@gmail.com>
2024-05-05 12:30:01 +0200

    Commit 5-5-2024

---
+++ src/main/java/wolladona/projecte_ed_v2/RegisterPageController.java
@@ -0,0 +1,13 @@
+package wolladona.projecte_ed_v2;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterPageController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
