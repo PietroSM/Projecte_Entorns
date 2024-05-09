982f25d00c45dd5339dd18f4be2bd4c8ce73b9ae
Pedro Sala <pedrosalamora@gmail.com>
2024-05-09 21:09:26 +0200

    Commit 9-5-2024

--- src/main/java/wolladona/projecte_ed_v2/LogInPageController.java
+++ src/main/java/wolladona/projecte_ed_v2/LogInPageController.java
@@ -1,43 +1,67 @@
 package wolladona.projecte_ed_v2;
 
 import PersonP.Users;
+import ProductP.Inventary;
+import ProductP.Product;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+/**
+ * Controller class responsible for the operation of the LogIn page
+ * (Classe controlador encarregada de tindre el funcionament de la pàgina de LogIn)
+ * @author Pedro Sala Mora
+ * @version 1
+ */
 public class LogInPageController implements Initializable {
-
+    @FXML
+    private Button BTNback;
     @FXML
     private TextField TFemale;
     @FXML
     private PasswordField PFpassword;
 
-    private Users usersListLoginPage;
 
+    private Users usersListLoginPage;
+    private Inventary inventaryList;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         usersListLoginPage = new Users();
-
+        inventaryList = new Inventary();
+        inventaryList.ReadFile();
     }
 
+    public Users getUsersListLoginPage() {
+        return usersListLoginPage;
+    }
+
+    /**
+     * This method is responsible for removing a Users object from the parent window and assigning it to our Users
+     * (Aquest métode s'encarrega de rebrer un objecte Users de la finestra pare i asignar-la al nostre Users)
+     * @param usersList The Users instance of the parent class
+     */
     public void setUsersListLoginPage(Users usersList){
         usersListLoginPage = usersList;
-        System.out.println("Hola");
-        System.out.println(usersListLoginPage.Showone(4));
     }
 
-
+    /**
+     * This method gives functionality to the login button to start and edit the corresponding window
+     * (Aquest métode, dona funcionalitat al botó d'iniciar sessió per a iniciar i redigirir a la finestra corresponent)
+     * @param actionEvent OnClick()
+     * @throws IOException
+     */
     public void button_click_login(ActionEvent actionEvent) throws IOException {
         String emailTF, passwordPF;
 
@@ -47,8 +71,8 @@ public class LogInPageController implements Initializable {
         if(!(emailTF.isEmpty()) &&
                 !(passwordPF.isEmpty()) &&
                 usersListLoginPage.CheckCredentials(emailTF,passwordPF)){
+            //Seller section
             if (usersListLoginPage.TypeOfUser(emailTF).equals("Seller")){
-                //System.out.println("Furula");
                 FXMLLoader loader = new FXMLLoader(WallaDona.class.
                         getResource("SellerPage-view.fxml"));
                 Scene scene = new Scene(loader.load());
@@ -57,12 +81,47 @@ public class LogInPageController implements Initializable {
                 stage.setScene(scene);
                 stage.initModality(Modality.APPLICATION_MODAL);
 
+                //Array aux
+                ArrayList<Product> listProduct = new ArrayList<>();
+
+                for (int i = 0; i < inventaryList.getList_Inventary().size(); i++) {
+                    if(inventaryList.getList_Inventary().get(i).getProductor()
+                            .equals(usersListLoginPage.PersonSearch(emailTF))){
+                        listProduct.add(inventaryList.getList_Inventary().get(i));
+                    }
+                }
+
+                //mirar per a borrar
+
 
                 SellerPageController controller = loader.getController();
                 controller.SetInfoSellerPage(usersListLoginPage,
+                        usersListLoginPage.PositionList(emailTF), listProduct);
+                stage.showAndWait();
+
+                //Retornar de la filla
+            //Client section
+            }else if(usersListLoginPage.TypeOfUser(emailTF).equals("Client")){
+                FXMLLoader loader = new FXMLLoader(WallaDona.class.
+                        getResource("MainPageUser-view.fxml"));
+                Scene scene = new Scene(loader.load());
+                Stage stage = new Stage();
+                stage.setTitle("Seller Page");
+                stage.setScene(scene);
+                stage.initModality(Modality.APPLICATION_MODAL);
+
+                MainPageUserViewController controller = loader.getController();
+                controller.SetInfoClientPage(usersListLoginPage,
                         usersListLoginPage.PositionList(emailTF));
                 stage.showAndWait();
-            }//fer per a redirigir a secció de clients
+
+                //retornar de la filla
+            }
         }
     }
+
+    public void button_click_back(ActionEvent actionEvent) {
+
+    }
+
 }
--- src/main/java/wolladona/projecte_ed_v2/MainPageUserViewController.java
+++ src/main/java/wolladona/projecte_ed_v2/MainPageUserViewController.java
@@ -1,10 +1,26 @@
 package wolladona.projecte_ed_v2;
 
+import PersonP.Users;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-public class MainPageUserViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainPageUserViewController implements Initializable {
+    @FXML
+    private TextField TFtype;
+    @FXML
+    private TextField TFlocation;
+    @FXML
+    private Button BTNsearch;
+    @FXML
+    private Button BTNcistella;
     @FXML
     private TableView table;
     @FXML
@@ -13,6 +29,26 @@ public class MainPageUserViewController {
     private TableColumn seasonC;
     @FXML
     private TableColumn amountColum;
+
+    private Users usersListUserPage;
+    private int pos;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void SetInfoClientPage(Users usersList, int pos){
+        usersListUserPage = usersList;
+        this.pos= pos;
+    }
+
+
+
+    public void button_click_Add_Cistella(ActionEvent actionEvent) {
+    }
+
+    public void button_click_search(ActionEvent actionEvent) {
+    }
 
 
 }
